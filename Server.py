5cefaf07cbd5c2ff8f14bd230a9e1165fc4cabb7
aungkaungmyat <amyat@purdue.edu>
2017-09-17 02:27:52 -0400

    everything going well

--- Server.py
+++ Server.py
@@ -64,7 +64,7 @@ def generateDerivativeQuestion():
     # eng = matlab.engine.start_matlab()
     eng1 = matlab.engine.connect_matlab('love')
     DQuestions = []
-    question = ['f = sqrt(x) - (1/(sqrt(x)));' , 'f = sqrt(x)*(x^2+1);' , 'f = 1/(7*x);' , 'f = (5*x^2)-(5*sqrt(x)-(3/x));', 'f = (3*x-2)*(2*x+1);', 'f = (5*x*sqrt(x))/2 + (1/2*sqrt(x));', 'f = 2/sqrt(x) + sqrt(x)/2' , 'f = -7/(1-x^3)', 'f = 12*x^2-6*x+2' , 'f = 2*x/3 + (6/x^3)' , 'f = sqrt(x) - 1/sqrt(x)']
+    question = ['y = sqrt(x) - (1/(sqrt(x)));' , 'y = sqrt(x)*(x^2+1);' , 'y = 1/(7*x);' , 'y = (5*x^2)-(5*sqrt(x)-(3/x));', 'y = (3*x-2)*(2*x+1);', 'y = (5*x*sqrt(x))/2 + (1/2*sqrt(x));', 'y = 2/sqrt(x) + sqrt(x)/2' , 'y = -7/(1-x^3)', 'y = 12*x^2-6*x+2' , 'y = 2*x/3 + (6/x^3)' , 'y = sqrt(x) - 1/sqrt(x)']
     # result = {}
     # question = DQuestions()
     # for counter in range(0, 4):
@@ -72,10 +72,10 @@ def generateDerivativeQuestion():
     # print(counter)
     eng1.eval("syms x;",nargout=0);
     eng1.eval(question[counter],nargout=0);
-    ans = eng1.eval('char(diff(f))',nargout=1)
-    ans1 = eng1.eval('char(-diff(f))',nargout=1)
-    ans2 = eng1.eval('char(diff(f)+1)',nargout=1)
-    ans3 = eng1.eval('char(diff(f)*1/2)',nargout=1)
+    ans = eng1.eval('char(diff(y))',nargout=1)
+    ans1 = eng1.eval('char(-diff(y))',nargout=1)
+    ans2 = eng1.eval('char(diff(y)+1)',nargout=1)
+    ans3 = eng1.eval('char(diff(y)*1/2)',nargout=1)
     DQuestions.append(question[counter])
     DQuestions.append(ans)
     DQuestions.append(ans1)
@@ -98,10 +98,19 @@ print('question is ', generateDerivativeQuestion())
 def connect(sid, environ):
     print("connect ", sid)
 
+
+# @sio.on('clientQ', namespace='/')
+# def connect(sid, environ):
+#     print(sid)
+#     print(environ)
+    # sio.emit('reply', generateDerivativeQuestion())
+
 @sio.on('questions', namespace='/')
 def connect(sid, environ):
     # print('here')
-    sio.emit('reply', generateDerivativeQuestion())
+    temp = generateDerivativeQuestion()
+    print(temp)
+    sio.emit('reply', temp)
 
     # print("connect ", sid)
     # print("environ ", environ);
