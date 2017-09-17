595f1eb7893feb8130d3cf346e4367aae0a3ee2c
aungkaungmyat <amyat@purdue.edu>
2017-09-17 05:49:07 -0400

    almost finialized

--- Server.py
+++ Server.py
@@ -20,6 +20,7 @@ app = Flask(__name__)
 #     return render_template('index.html')
 DQuestions = []
 IQuestions = []
+AQuestions = []
 # class DQuestions(object):
 #     correct = JSONEncoder().encode({
 #         "q" : "",
@@ -28,43 +29,58 @@ IQuestions = []
 #     false1 = ""
 #     false2 = ""
 #     false3 = ""
+def generateAlgebraQuestion():
+    eng1 = matlab.engine.connect_matlab('love')
+    AQuestions = []
+    question = ['sqrt(32*x^3)*sqrt(2*x^3);' , 'sqrt(32*x^5)*sqrt(2*x^3);' , '(-2*x-3)*(-4*x^2+3*x+7);' , 'x+6-(9*3);', 'x*(7+12)-12/2;', '(x/7)*(x/5+9);', 'x-5+9*(3/4)+6*2' , 'x+9-5/4', '9*3-6+6*x' , 'x/7+6*x-9+2' , '5*x+(x/7)-2']
+    counter = random.randrange(0,9);
+    eng1.eval("syms x;",nargout=0);
+    eng1.workspace['y'] = question[counter];
+    ans = eng1.eval('char(eval(y))',nargout=1);
+    # print('ans ',ans)
+    ans1 = eng1.eval('char(-eval(y))',nargout=1)
+    ans2 = eng1.eval('char(eval(y)+1)',nargout=1)
+    ans3 = eng1.eval('char(eval(y)*1/2)',nargout=1)
+    AQuestions.append(question[counter])
+    AQuestions.append(ans)
+    AQuestions.append(ans1)
+    AQuestions.append(ans2)
+    AQuestions.append(ans3)
+    eng1.quit()
+    return AQuestions
+
+
+
 def generateIntegralQuestion():
-    # eng = matlab.engine.start_matlab()
     eng1 = matlab.engine.connect_matlab('love')
     IQuestions = []
-    IQuestions = ['f = sqrt(x) - (1/(sqrt(x)));' , 'f = sqrt(x)*(x^2+1);' , 'f = 1/(7*x);' , 'f = (5*x^2)-(5*sqrt(x)-(3/x));', 'f = (3*x-2)*(2*x+1);', 'f = (5*x*sqrt(x))/2 + (1/2*sqrt(x));', 'f = 2/sqrt(x) + sqrt(x)/2' , 'f = -7/(1-x^3)', 'f = 12*x^2-6*x+2' , 'f = 2*x/3 + (6/x^3)' , 'f = sqrt(x) - 1/sqrt(x)']
+    question = ['sqrt(x) - (1/(sqrt(x)));' , 'sqrt(x)*(x^2+1);' , 'sin(x)/(7*x);' , '(5*x^2)-(5*sqrt(x)-(3/x));', 'tan(3*x-2)*(2*x+1);', '(5*x*sqrt(x))/2 + (1/2*sqrt(x));', '2*cos(x)/sqrt(x) + sqrt(x)/2' , '-7*x^5/(1-x^3)', '12*x^2-6*x+2*sec(x)' , '2*x/(3*sin(x)) + (6/x^3)' , 'sqrt(x) - 1/sqrt(x)']
     # result = {}
     # question = DQuestions()
     # for counter in range(0, 4):
     counter = random.randrange(0,9);
     # print(counter)
     eng1.eval("syms x;",nargout=0);
-    eng1.eval(IQuestions[counter],nargout=0);
-    ans = eng1.eval('char(diff(f))',nargout=1)
-    ans1 = eng1.eval('char(-diff(f))',nargout=1)
-    ans2 = eng1.eval('char(diff(f)+1)',nargout=1)
-    ans3 = eng1.eval('char(diff(f)*1/2)',nargout=1)
-    IQuestions.append(IQuestions[counter])
+    eng1.workspace['y'] = question[counter];
+    ans = eng1.eval('char(int(y,x))',nargout=1);
+    ans1 = eng1.eval('char(-int(y,x))',nargout=1)
+    ans2 = eng1.eval('char(int(y,x)+1)',nargout=1)
+    ans3 = eng1.eval('char(int(y,x)*1/2)',nargout=1)
+    IQuestions.append(question[counter])
     IQuestions.append(ans)
     IQuestions.append(ans1)
     IQuestions.append(ans2)
     IQuestions.append(ans3)
-        # a = eng1.eval('sqrt(5)')
-        # print(a)
-        # res
-        # print(ans)
     eng1.quit()
-
-
     return IQuestions
 
-
+# print('Integral ', generateIntegralQuestion())
 
 def generateDerivativeQuestion():
     # eng = matlab.engine.start_matlab()
     eng1 = matlab.engine.connect_matlab('love')
     DQuestions = []
-    question = ['y = sqrt(x) - (1/(sqrt(x)));' , 'y = sqrt(x)*(x^2+1);' , 'y = 1/(7*x);' , 'y = (5*x^2)-(5*sqrt(x)-(3/x));', 'y = (3*x-2)*(2*x+1);', 'y = (5*x*sqrt(x))/2 + (1/2*sqrt(x));', 'y = 2/sqrt(x) + sqrt(x)/2' , 'y = -7/(1-x^3)', 'y = 12*x^2-6*x+2' , 'y = 2*x/3 + (6/x^3)' , 'y = sqrt(x) - 1/sqrt(x)']
+    question = ['y = sqrt(x) - (1/(sqrt(x)));' , 'y = sqrt(x)*(x^2+1);' , 'y = 1/(7*x);' , 'y = (5*x^2)-(5*sqrt(x)-(3/x));', 'y = (sin(x))*(2*x+1);', 'y = (5*x*sqrt(x))/2 + (1/2*sqrt(x));', 'y = 2*cos(x)/sqrt(x) + sqrt(x)/2' , 'y = -7/(1-x^3)', 'y = 12*x^2-6*x+2' , 'y = 2*x^2/3 + (6/x^3)' , 'y = sqrt(x) - 1/sqrt(x)']
     # result = {}
     # question = DQuestions()
     # for counter in range(0, 4):
@@ -92,7 +108,7 @@ def generateDerivativeQuestion():
 
 
 
-print('question is ', generateDerivativeQuestion())
+# print('question is ', generateDerivativeQuestion())
 
 @sio.on('connect', namespace='/chat')
 def connect(sid, environ):
@@ -109,8 +125,22 @@ def connect(sid, environ):
 def connect(sid, environ):
     # print('here')
     temp = generateDerivativeQuestion()
-    print(temp)
+    # print(temp)
     sio.emit('reply', temp)
+
+@sio.on('questions2', namespace='/')
+def connect(sid, environ):
+    # print('here')
+    temp1 = generateIntegralQuestion()
+    # print(temp1)
+    sio.emit('reply2', temp1)
+#
+@sio.on('questions3', namespace='/')
+def connect(sid, environ):
+    # print('here')
+    temp2 = generateAlgebraQuestion()
+    # print(temp2)
+    sio.emit('reply3', temp2)
 
     # print("connect ", sid)
     # print("environ ", environ);
